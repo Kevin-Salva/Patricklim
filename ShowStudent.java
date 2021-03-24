f3b74f2e9efcbac6fff3e15c142c794b1f7eb1eb
patrick <patrick@gmail.com>
2021-03-24 14:41:57 +0800

    first

---
+++ ShowStudent.java
@@ -0,0 +1,15 @@
+package com.company;
+import java.util.Scanner;
+public class ShowStudent {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Id");
+        String Id = sc.next();
+        System.out.println("Enter number of credits: ");
+        int cs = sc.nextInt();
+        System.out.println("Enter number of points: ");
+        int p = sc.nextInt();
+        Student s = new Student( );
+        System.out.println(s);
+    }
+}
